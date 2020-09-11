58e28b5b087796a787470f776990d3498ad90f0a
Cherry Chu <ccc2207@columbia.edu>
2020-09-11 16:46:46 -0400

    Implemented /startgame and /joingame endpoints.

--- hw1/src/main/java/controllers/PlayGame.java
+++ hw1/src/main/java/controllers/PlayGame.java
@@ -4,17 +4,22 @@ import io.javalin.Javalin;
 import java.io.IOException;
 import java.util.Queue;
 import org.eclipse.jetty.websocket.api.Session;
+import models.Player;
+import models.GameBoard;
+import com.google.gson.Gson;
 
 class PlayGame {
 
   private static final int PORT_NUMBER = 8080;
 
   private static Javalin app;
-
+  
   /** Main method of the application.
    * @param args Command line arguments
    */
   public static void main(final String[] args) {
+	
+	GameBoard board = new GameBoard();
 
     app = Javalin.create(config -> {
       config.addStaticFiles("/public");
@@ -25,27 +30,29 @@ class PlayGame {
       ctx.result(ctx.body());
     });
 
-    /**
-     * Please add your end points here.
-     * 
-     * 
-     * 
-     * 
-     * Please add your end points here.
-     * 
-     * 
-     * 
-     * 
-     * Please add your end points here.
-     * 
-     * 
-     * 
-     * 
-     * Please add your end points here.
-     * 
-     */
     app.get("/newgame", ctx -> {
     	ctx.redirect("/tictactoe.html");
+    });
+ 
+    app.post("/startgame", ctx -> {
+    	Player p1 = new Player();
+    	board.setP1(p1);
+        p1.setType(ctx.formParam("type").charAt(0));
+        p1.setId(1);
+        ctx.result(new Gson().toJson(board));
+    });
+    
+    app.get("/joingame", ctx -> {
+    	Player p2 = new Player();
+    	board.setP2(p2);
+    	if (board.getP1().getType() == 'X')
+    		p2.setType('O');
+    	else
+    		p2.setType('X');
+    	p2.setId(2);
+        ctx.redirect("/tictactoe.html?p=2");
+        board.setGameStarted(true);
+        sendGameBoardToAllPlayers(new Gson().toJson(board));
     });
 
     // Web sockets - DO NOT DELETE or CHANGE
@@ -63,6 +70,8 @@ class PlayGame {
         sessionPlayer.getRemote().sendString(gameBoardJson);
       } catch (IOException e) {
         // Add logger here
+    	  e.printStackTrace();
+    	  //LOG.log("Could not do what I wanted", e);
       }
     }
   }
--- hw1/src/main/java/models/GameBoard.java
+++ hw1/src/main/java/models/GameBoard.java
@@ -8,12 +8,40 @@ public class GameBoard {
 
   private boolean gameStarted;
 
-  private int turn;
+  private int turn = 1;
 
-  private char[][] boardState;
+  private char[][] boardState = new char[3][3];
 
   private int winner;
 
   private boolean isDraw;
+  
+  public Player getP1() {
+	  return p1;
+  }
+  
+  public Player getP2() {
+	  return p2;
+  }
+  
+  public void setP1(Player p1) {
+	  this.p1 =  p1;
+  }
+  
+  public void setP2(Player p2) {
+	  this.p2 = p2;
+  }
+  
+  public void switchTurn() {
+	  turn = turn % 2 + 1;
+  }
+  
+  public boolean isGameStarted() {
+	  return gameStarted; 
+  }
+	
+  public void setGameStarted(boolean gameStarted) {
+	  this.gameStarted = gameStarted;
+  }
 
 }
